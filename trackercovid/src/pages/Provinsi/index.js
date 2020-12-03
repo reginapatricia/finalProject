25859a49d002c574e2715101f72d9231e5468f12
reginapatricia <s21810085@student.unklab.ac.id>
2020-12-03 19:34:12 +0800

    Pages commit

---
+++ trackercovid/src/pages/Provinsi/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Provinsi = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Provinsi;
---
+++ trackercovid/src/pages/index.js
@@ -0,0 +1,6 @@
+import Global from './Global'
+import Indonesia from './Indonesia'
+import Provinsi from './Provinsi'
+
+export {Global, Indonesia, Provinsi};
+
